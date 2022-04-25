433cca908b26a288547f5bba2fdd3b913b38c9aa
Vladchegg <vlad0277@mail.ru>
2022-04-25 15:11:39 +0300

    решенные задачи 7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,21 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+    // Задача 2
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName1);
+
+    // Задача 3
+        fullName = "Иванов Семён Семёнович";
+        String fullName3 = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + fullName3);
+
     }
 }
